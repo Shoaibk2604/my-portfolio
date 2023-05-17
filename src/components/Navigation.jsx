823f4f55233b9912d73c4fcdcc610f0c81472e74
Shoaib <Shoaibk909@gmail.com>
2023-05-17 18:02:19 +0530

    navigation-done

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navigation = () => {
+  return (<div className=" navigation-container">
+
+    <div className="absolute inset-y-0 right-12 navigation-wrapper uppercase ">
+      <div className="nav-btns ">Home</div>
+      <div className="nav-btns">About</div>
+      <div className="nav-btns">Projects</div>
+      <div className="nav-btns">help</div>
+      <div className="nav-btns">contact</div>
+    </div>
+  </div>
+  );
+};
+
+export default Navigation;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+  return (
+    <div className="home-wrapper ">
+      <div className="container ">
+        <Navigation />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
