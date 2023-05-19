3c79cd420f605dc0f22dbb94997af437524652e9
Shoaib <Shoaibk909@gmail.com>
2023-05-19 16:08:33 +0530

    home-layout-done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,11 @@ import profile from "../Assests/profile_img.png";
 const Profile = () => {
   return (
     <>
+    <div className="profile-info">
+    <p>I like to craft solid and scalable frontend products with great user experiences.</p>
+    </div>
       <div className="profile-container">
+        <div className="profile-box"></div>
       <div className="profile-title text-5xl uppercase">
         <div className="title-one">
           <h3>Frontend</h3>
@@ -12,7 +16,6 @@ const Profile = () => {
           <h3 >Developer</h3>
         </div>
       </div>
-        <div className="profile-box"></div>
         <div className="profile-img">
           <img src={profile} alt="Profile picture" />
         </div>
