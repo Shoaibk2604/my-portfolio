131c9d1b7c9d7aabb4f6c31b437e006ba46250dc
Shoaib <Shoaibk909@gmail.com>
2023-05-19 12:18:05 +0530

    title-done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import profile from "../Assests/profile_img.png";
 const Profile = () => {
   return (
-    <div className="profile-container">
-      <div className="profile-box"></div>
-      <div className="profile-img">
-        <img src={profile} alt="" />
+    <>
+      <div className="profile-container">
+      <div className="profile-title text-5xl uppercase">
+        <div className="title-one">
+          <h3>Frontend</h3>
+        </div>
+        <div className="title-two">
+          <h3 >Developer</h3>
+        </div>
       </div>
-    </div>
+        <div className="profile-box"></div>
+        <div className="profile-img">
+          <img src={profile} alt="Profile picture" />
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navigation from "../components/Navigation";
 import Profile from "../components/Profile";
 
 const Home = () => {
+  
   return (
       <div className="container ">
         <Profile/>
