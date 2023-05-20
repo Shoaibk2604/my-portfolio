2afae9f2b9f464145f05dcfec461472f2642f0a1
Shoaib <Shoaibk909@gmail.com>
2023-05-20 17:42:25 +0530

    project-page-grid

--- src/pages/Projects/Project.jsx
+++ src/pages/Projects/Project.jsx
@@ -5,8 +5,18 @@ const Project = () => {
   return (
     <>
       <Transition />
-      <div className="project-container">
-        <div className="project-page-heading "><h2 className="uppercase text-5xl">Personal projects</h2></div>
+      <div className="project-container mx-8">
+        <div className="project-page-heading ">
+          <h2 className="uppercase text-6xl grid place-content-center mt-12 text-indigo-200">
+            Personal projects
+          </h2>
+        </div>
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-12 place-content-around my-8 place-items-center">
+<div className="border-2  h-60">01</div>
+<div className="border-2  h-60">02</div>
+<div className="border-2  h-60">03</div>
+<div className="border-2  h-60">04</div>
+        </div>
       </div>
     </>
   );
