d6fa9e214126bbd34307e0cb79e21eb7871bf183
Shoaib <Shoaibk909@gmail.com>
2023-05-30 13:08:48 +0530

    corrections

--- src/components/BackgroundDesign.jsx
+++ src/components/BackgroundDesign.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const BackgroundDesign = () => {
   return (
-    <>
+    <div className="hidden sm:block">
       <div class="bg-design bg-design-1 w-20 h-20 "></div>
       <div class="bg-design-1 w-8 h-8 bottom-14 left-4"></div>
       <div class="bg-design-1 w-4 h-4 bottom-20 left-14"></div>
       <div class="bg-design-right bg-design-1 w-20 h-20 "></div>
       <div class="bg-design-1 w-8 h-8 top-14 right-4"></div>
       <div class="bg-design-1 w-4 h-4 top-20 right-14"></div>
-    </>
+    </div>
   );
 };
 
